8fd7102a91013696c9a875b9be30a731a39620eb
brandon <brandon@brandonalexander.io>
2026-03-03 20:15:16 -0600

    updates

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,4 +3,18 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "rust_analyzer", "emmylua_ls", "vimls"}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Custom LSP hover styling
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "rounded",
+    width = 60,
+    height = 20,
+  }
+)
+
+-- Custom highlight colors for hover window
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89b4fa" })
+
+-- read :h vim.lsp.config for changing options of lsp servers
